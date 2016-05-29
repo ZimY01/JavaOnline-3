a4259c85dddf544d9fbe3f36d20576dc23dd05a7
ZimY01 <zimy01@gmail.com>
2016-05-30 00:50:47 +0300

    task3

---
+++ src/module2and3/task3/Guitar.java
@@ -0,0 +1,7 @@
+package module2and3.task3;
+
+/**
+ * Created by zimy0 on 30.05.2016.
+ */
+public class Guitar extends MusicInstruments {
+}
---
+++ src/module2and3/task3/MusicShop.java
@@ -0,0 +1,18 @@
+package module2and3.task3;
+
+import java.util.List;
+
+/**
+ * Created by zimy0 on 30.05.2016.
+ */
+public class MusicShop {
+    private List<MusicInstruments> musicInstruments;
+
+    public List<MusicInstruments> getMusicInstruments() {
+        return musicInstruments;
+    }
+
+    public void setMusicInstruments(List<MusicInstruments> musicInstruments) {
+        this.musicInstruments = musicInstruments;
+    }
+}
