5c9c4bde69ac29ba3fffcd9a890dfc2ccdc8530d
ZimY01 <zimy01@gmail.com>
2016-05-29 23:14:33 +0300

    Initial commit

---
+++ src/module2and3/task1/Directory.java
@@ -0,0 +1,18 @@
+package module2and3.task1;
+
+import java.util.List;
+
+/**
+ * Created by zimy0 on 29.05.2016.
+ */
+public class Directory {
+    private List<File> files;
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
---
+++ src/module2and3/task1/File.java
@@ -0,0 +1,7 @@
+package module2and3.task1;
+
+/**
+ * Created by zimy0 on 29.05.2016.
+ */
+public class File {
+}
