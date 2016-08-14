f61a0d9c744dcafb5511fab5f65088f97aba3bfe
ZimY01 <zimy01@gmail.com>
2016-08-14 14:08:00 +0300

    Module5

--- src/module5/SortMinMax.java
+++ src/module5/SortMinMax.java
@@ -1,15 +1,18 @@
 package module5;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortMinMax {
 
     public static void main(String[] args) {
-        int array[] = {2,5,6,7,8,1,2,0,3};
+        Integer array[] = {2,5,6,7,8,1,2,0,3};
 
-        Arrays.sort(array);
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(array));
+        Collections.sort(arrayList);
 
-        System.out.println("The minimal number is: " + array[0]);
-        System.out.println("The maximal number is: " + array[array.length - 1]);
+        System.out.println("The minimal number is: " + Collections.min(arrayList));
+        System.out.println("The maximal number is: " + Collections.max(arrayList));
     }
 }
